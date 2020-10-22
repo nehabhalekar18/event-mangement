06671896ddc0cf966f931d06dff6a158f4095e9d
Neha Bhalekar <nehabhalekar18@gmail.com>
2020-10-22 11:53:16 +0200

    Landing page completed

--- event-mgnt/src/pages/LandingPage.js
+++ event-mgnt/src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import { Col, Container, Row,Button } from "react-bootstrap";
 import Header from "../components/Header";
+import "./LandingPage.css"
 class LandingPage extends Component {
   render() {
     return (
@@ -9,24 +10,22 @@ class LandingPage extends Component {
         <Container style={{ textAlign: "center", padding: "5% 0" }}>
           <Row>
             <Col lg={12}>
-              <h1>Evento</h1>
+              <h1>Welcome Evento</h1>
             </Col>
           </Row>
           <br />
+          <br/>
           <Row>
-            <Col lg={{ span: 6, offset: 4 }}>
-              <InputGroup>
-                <FormControl
-                  placeholder="Username"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
-              </InputGroup>
-            </Col>
-            <Col lg={12}>
-              <input type="password" />
+            <Col lg={{ span: 12 }}>
+              Evento is here to assist you with all the hard work involved in
+              planning an event. Manage your customersand events with the help
+              of Evento.
             </Col>
           </Row>
+          <br/>
+          <br/>
+          <br/>
+          <Button className="loginBtn">Login</Button>
         </Container>
       </div>
     );
